5084f1934b324626d26bcc39392ac5daead1ac21
ymyldz <yusfmertyildiz@gmail.com>
2023-05-18 19:37:51 +0300

    Initial Commit

---
+++ LoginProtocolOrianted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginProtocolOrianted
+//
+//  Created by Yusuf Mert Yıldız on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoginProtocolOriantedUITests/LoginProtocolOriantedUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoginProtocolOriantedUITestsLaunchTests.swift
+//  LoginProtocolOriantedUITests
+//
+//  Created by Yusuf Mert Yıldız on 18.05.2023.
+//
+
+import XCTest
+
+final class LoginProtocolOriantedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
